c273a8a517b2e95c780ae624a707484255c503df
mavese <mavese@live.com>
2017-09-12 21:33:51 -0500

    Adding main code to master.

---
+++ hw.py
@@ -0,0 +1,64 @@
+#Matthew Mantese
+
+import urllib
+from collections import deque
+
+def getLinks(url,baseurl="http://secon.utulsa.edu/cs2123/webtraverse/"):
+    """
+    Input: url to visit, Boolean absolute indicates whether URLs should include absolute path (default) or not
+    Output: list of pairs of URLs and associated text
+    """
+    #import the HTML parser package 
+    try:
+        from BeautifulSoup import BeautifulSoup
+    except:
+        print 'You must first install the BeautifulSoup package for this code to work'
+        raise
+    #fetch the URL and load it into the HTML parser
+    soup = BeautifulSoup(urllib.urlopen(url).read())
+    #pull out the links from the HTML and return
+    return [baseurl+a["href"].encode('ascii','ignore') for a in soup.findAll('a')]
+
+def print_dfs(url):
+    """
+    Print all links reachable from a starting **url** 
+    in depth-first order
+    """
+    #
+
+def print_bfs(url):
+    """
+    Print all links reachable from a starting **url** 
+    in breadth-first order
+    """
+    #
+
+    
+def find_shortest_path(url1,url2):
+    """
+    Find and return the shortest path
+    from **url1** to **url2** if one exists.
+    If no such path exists, say so.
+    """
+    #
+
+def find_max_depth(start_url):
+    """
+    Find and return the URL that is the greatest distance from start_url, along with the sequence of links that must be followed to reach the page.
+    For this problem, distance is defined as the minimum number of links that must be followed from start_url to reach the page.
+    """
+    #
+
+
+if __name__=="__main__":
+    starturl = "http://secon.utulsa.edu/cs2123/webtraverse/index.html"
+    print "*********** (a) Depth-first search   **********"
+    print_dfs(starturl)
+    print "*********** (b) Breadth-first search **********"
+    print_bfs(starturl)
+    print "*********** (c) Find shortest path between two URLs ********"
+    find_shortest_path("http://secon.utulsa.edu/cs2123/webtraverse/index.html","http://secon.utulsa.edu/cs2123/webtraverse/wainwright.html")
+    find_shortest_path("http://secon.utulsa.edu/cs2123/webtraverse/turing.html","http://secon.utulsa.edu/cs2123/webtraverse/dijkstra.html")
+    print "*********** (d) Find the longest shortest path from a starting URL *****"
+    find_max_depth(starturl)
+    print getLinks(starturl)
